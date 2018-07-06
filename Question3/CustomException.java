cf48a24db383699eeb3b2b968777fad7cdb74a07
Aayushi <aayushi@zemosolabs.com>
2018-07-06 12:52:26 +0530

    Completed third part

---
+++ Question3/CustomException.java
@@ -0,0 +1,51 @@
+/**
+ * My custom exception class.
+ */
+class MyCustomException extends Exception
+{
+  public MyCustomException(String message)
+  {
+    super(message);
+  }
+}
+
+/**
+ * Our test class to demonstrate custom exception.
+ */
+class Foo
+{
+  public String getNum(int i)throws MyCustomException
+  {
+    if (i == 0)
+    {
+      // throw custom exception
+      throw new MyCustomException("Anything but zero ...");
+    }
+    else
+    {
+      return "Thanks";
+    }
+  }
+}
+
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    // create a new foo
+    Foo foo = new Foo();
+    
+    try
+    {
+      // intentionally throw our custom exception by
+      // calling getBar with a zero
+      String bar = foo.getNum(0);
+    }
+    catch (MyCustomException e)
+    {
+      // print the stack trace
+      e.printStackTrace();
+    }
+  }
+}
